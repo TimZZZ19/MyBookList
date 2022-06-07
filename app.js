c471838d108ff32f5ae68317184ed8d3866a223a
TimZZZ19 <53802591+TimZZZ19@users.noreply.github.com>
2022-06-06 20:25:29 -0500

    added UI

---
+++ app.js
@@ -0,0 +1,11 @@
+// Book Class: Represents a Book
+
+// UI Class: Handles UI Tasks
+
+// Store Class: Hanldes Storage
+
+// Event: Displays Books
+
+// Event: Adds a Book
+
+// Event: Removes a Book
